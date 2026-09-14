2ea3dbcdb889e6254f86adafec8971060d12f877
juan1jose3 <moralesbenitezjuanjose@gmail.com>
2026-09-13 19:26:06 -0500

    main page done

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,3 +1,46 @@
+import beefImage from "../images/beef.jpg";
+import chickenImage from "../images/chicken.jpg";
+import pastaImage from "../images/pasta.jpg";
+
+
+function renderCards(menuContainer){
+    for(let cardItem = 0; cardItem < 3; cardItem++){
+        const item = document.createElement("div");
+        const itemTitle = document.createElement("h2");
+
+        const textWrapper = document.createElement("div");
+        textWrapper.classList.add("text-wrapper");
+        const itemText = document.createElement("p");
+        const image = document.createElement("img");
+        image.classList.add("card-image");
+
+        textWrapper.appendChild(itemText);
+
+        item.classList.add("card");
+        item.appendChild(itemTitle);
+        item.appendChild(image);
+        item.appendChild(textWrapper);
+
+        item.classList.add("menu-card");
+
+        if(cardItem === 0){
+            itemTitle.textContent = "Beef Steak";
+            image.src = beefImage;
+            itemText.textContent = "Prime-cut beef charbroiled to juicy perfection, served sizzling with a rich, savory reduction."
+        }else if(cardItem === 1){
+            itemTitle.textContent = "Roasted Chicken";
+            image.src = chickenImage;
+            itemText.textContent = "Herb-marinated free-range chicken roasted golden and crisp, complemented by pan drippings.";
+        }else{
+            itemTitle.textContent = "Spaghetti";
+            image.src = pastaImage;
+            itemText.textContent = "Artisanal pasta tossed in a velvety garlic-infused sauce with fresh seasonal herbs and aged parmesan.";
+        }
+
+        menuContainer.appendChild(item);
+    }
+}
+
 function renderHome(){
     const mainContainer = document.querySelector(".main-container");
 
@@ -13,13 +56,22 @@ function renderHome(){
     
     const mainTileWrapper = document.createElement("div");
     mainTileWrapper.classList.add("main-title");
-    mainTileWrapper.textContent = "Hello Food";
+    mainTileWrapper.textContent = "The Rich Flavor";
 
     const menuSection = document.createElement("div");
-    menuSection.textContent = "Menu";
+    const menuTitle = document.createElement("h1");
+    menuTitle.textContent = "Today's Special";
     menuSection.classList.add("menu-section");
+    menuSection.appendChild(menuTitle);
 
-    
+
+
+    const menuCardContainer = document.createElement("div");
+    menuCardContainer.classList.add("menu-card-container");
+    menuSection.appendChild(menuCardContainer);
+
+    renderCards(menuCardContainer);
+
     darkWrapper.appendChild(mainTileWrapper);
     bannerContainer.appendChild(darkWrapper);
     
