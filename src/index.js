674427610c528f35f3694d8337c4138d6151fcdd
juan1jose3 <moralesbenitezjuanjose@gmail.com>
2026-09-15 17:01:49 -0500

    I have to modify the navbar and add start doing the menu page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import "./styles.css";
 import renderHome from "./pages/home.js";
+import renderMenu from "./pages/menu.js";
+
+const navBar = document.querySelector(".nav-bar");
+const mainContainer = document.querySelector(".main-container");
 
 renderHome();
+
+navBar.addEventListener("click",event =>{
+    const button = event.target.closest("button");
+
+    if(button.classList.contains("menu")){
+        mainContainer.textContent = "";
+        renderMenu();
+    }else if(button.classList.contains("home")){
+        mainContainer.textContent = "";
+        renderHome();
+    }
+
+});
